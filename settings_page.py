6fac77002f4474cf3dcfdff9be30f6864a6d1d4d
Mehrab Hossain <mehrab.cse16@gmail.com>
2024-03-04 15:30:56 +0600

    Add Timer Scrollbar

--- settings_page.py
+++ settings_page.py
@@ -18,6 +18,13 @@ class SettingsPage():
 
         # Buttons
         self.back_button = Button(self.screen_rect.right - 106, 64 , 42, 43)
+
+        # Timer settings
+        self.timer_duration = self.settings.timer_duration / 100
+        self.timer_scroll_bar = pygame.Rect(self.screen_rect.centerx - 170, self.screen_rect.centery - 70, 430, 5)
+        self.timer_indicator_x = self.timer_scroll_bar.x + self.timer_scroll_bar.width * self.timer_duration
+        self.timer_dragging = False
+
     
     def handle_events(self, events):
         # handle keyboard and mouse events
@@ -30,7 +37,42 @@ class SettingsPage():
                 if self.back_button.rect.collidepoint(mouse_pos):  
                     return 'main_page'
                 
+                if self.timer_scroll_bar.collidepoint(mouse_pos):
+                    self.timer_dragging = True
+            elif event.type == pygame.MOUSEBUTTONUP:
+                self.timer_dragging = False
+
+
+        if self.timer_dragging:
+            mouse_pos_x, _ = pygame.mouse.get_pos()
+            if mouse_pos_x < self.timer_scroll_bar.left + 10:
+                self.timer_duration = 0.10
+            elif mouse_pos_x > self.timer_scroll_bar.right:
+                self.timer_duration = 1
+            else:
+                self.timer_duration = (mouse_pos_x - self.timer_scroll_bar.left) / self.timer_scroll_bar.width
+
+            
+        self.settings.timer_duration = self.timer_duration
+
+
     def draw(self):
         # Draw the background image
         self.screen.blit(self.background_img, self.background_img_rect)
-        # self.back_button.draw(self.screen)    
+        
+        # Draw the timer minutes
+        # Draw the timer percentage
+        if self.timer_duration <= 0.10:
+            self.timer_text = self.settings.sub_title_font.render(f"{int(self.timer_duration * 100)} seconds", True, "red")
+        else:
+            self.timer_text = self.settings.sub_title_font.render(f"{int(self.timer_duration * 100)} seconds", True, "#4CAEC0")
+        
+        self.timer_text_rect = self.timer_text.get_rect()
+        self.timer_text_rect.right = self.timer_scroll_bar.right + 200
+        self.timer_text_rect.centery = self.timer_scroll_bar.centery
+        self.screen.blit(self.timer_text, self.timer_text_rect)
+
+        # Draw the timer bar
+        pygame.draw.rect(self.screen, "#4CAEC0", self.timer_scroll_bar)
+        self.timer_indicator_x = self.timer_scroll_bar.x + self.timer_scroll_bar.width * self.timer_duration
+        pygame.draw.circle(self.screen, "#4CAEC0", (int(self.timer_indicator_x), int(self.timer_scroll_bar.centery)), 10)
